ad9e8a3a6bb0fb52a76d0bead6d12e118b26033e
Kagan Demirer <demirerkagan0808@gmail.com>
2023-10-10 13:08:18 +0200

    Deploy py

--- deploy.py
+++ deploy.py
@@ -1,3 +1,3 @@
 import subprocess
-command = 'ssh demirer.info@ssh.strato.de "cd /mnt/web310/b1/02/512442602/htdocs/demirer.info && git pull"'
+command = 'ssh demirer.info@ssh.strato.de "cd /mnt/web310/b1/02/512442602/htdocs/demirer.info && git pull origin main"'
 process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
