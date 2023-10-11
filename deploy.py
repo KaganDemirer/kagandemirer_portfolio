e3b989c8f3d7a097e258bbfeeaa37a410b3f6fd1
Kagan Demirer <demirerkagan0808@gmail.com>
2023-10-11 14:06:01 +0200

    remove preRelease

--- deploy.py
+++ deploy.py
@@ -1,4 +1,3 @@
 import subprocess
 command = 'ssh demirer.info@ssh.strato.de "cd /mnt/web310/b1/02/512442602/htdocs/demirer.info && git pull origin main"'
-command = 'ssh demirer.info@ssh.strato.de "cd /mnt/web310/b1/02/512442602/htdocs/demirer.info.preRelease/demirer.info && git pull"'
 process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
