3e58221b57c77fb0852138f59fe4d992d651200e
Kagan Demirer <demirerkagan0808@gmail.com>
2023-10-13 11:50:05 +0200

    Handy Version und Karriere

--- KaganDemirer/steckbrief.js
+++ KaganDemirer/steckbrief.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+    const karriere_line_elements = document.getElementsByClassName('karriere-line-element');
+    for (let i = 0; i < karriere_line_elements.length; i++) {
+        observer.observe(karriere_line_elements[i]);
+    }
     setTimeout(() => {
         typeWriter("Hallo!")
         .then(() => {
@@ -16,8 +20,20 @@ $(document).ready(function() {
         })
         .then(() => typeWriter("Ich bin Kagan Demirer, 23 Jahre alt und komme aus Walheim. Ich bin ein sehr offener und freundlicher Mensch. Ich bin sehr motiviert und lerne gerne neue Dinge. Ich bin sehr sportlich und spiele gerne Fussball. Ich bin sehr interessiert an der Informatik und möchte mich in diesem Bereich weiterentwickeln."));
     }, 4500); // Wait for 2 seconds before starting the first typeWriter
-    
 });
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('opacity-animation');
+        return; // if we added the class, exit the function
+        }
+
+        // We're not intersecting, so remove the class!
+        entry.target.classList.remove('opacity-animation');
+    });
+});
+
 
 
 function typeWriter(txt) {
